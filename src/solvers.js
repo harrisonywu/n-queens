00e982b69cb005b088e508761664ec0d627918b1
harrisonywu <harrisonywu@gmail.com>
2019-02-28 16:48:29 -0800

    Produce correct solution for solvers.js, findNRooksSolution, still failing mocha test

--- src/solvers.js
+++ src/solvers.js
@@ -5,49 +5,40 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; 
-  var board = makeEmptyMatrix(n);
-  var boardLength = board.get('n');
-
-  board.attributes[0][0] = board.set('1');
-
-  for (var r = 0; r < boardLength; r++) {
-    var boardRow = board.attributes[r];
-      addSafeRook(r);
+  var solution = new Board({n:n});
+  var boardLength = n; //board.get('n');
+  //board.attributes[0][0] = board.set('1');
+  var addSafeRook = function(c) {
+    var bigArray = this;
+    solution.togglePiece(r, c);
+    debugger;
+    if ( solution.hasRowConflictAt(c) || solution.hasColConflictAt(c) ) {
+      debugger;
+      solution.togglePiece(r, c);
+      if (c < boardLength) {
+        addSafeRook( c + 1 );
+      }
+      else {
+        console.log('no solutions, go back up the tree');
+        return;
+      }
+      //recursively call addSafeRook? if (all positions have conflicts) exit recursion console log 'no soln in htis row' / else addSafeRook(n+1)
+      //var index=0; do { addSafeRook(index+1); } while(colIndex<n)
+    } else {
+      console.log('Set a safe rook at position ' + r + ',' + c);
+      return;
+    }
+};
   
+  for (var r = 0; r < boardLength; r++) {
+    var boardRow = solution.attributes[r];  
+    addSafeRook.call(solution.attributes,r);
   }
   
-var addSafeRook = function (colIndex) {
-  // Pseudocode: 
-  // helper function will check the rows. 
-  // storage var positionSquare = boardRow[colIndex];
-  // add r row rook in a particular square. 
-  // and check if there are conflicts 
-  // then return rook position. 
   
-};
-
-  // input n for the nxn board size, and n number of rook pieces
-  // output solution is a matrix that is an object with row arrays, board[0][0] would specify each square
-  // constraints, edge cases; only need one possible arrangement of rooks
-  // Justification/Explanation: we want to return a possible board state where the rooks don't threaten each other
-  // note: we will implement our solution with step by step checking through the board
-
-  // Pseudocode
-  // first build an empty nxn board (see Board.js), all 0s, pseudoclassical. This empty board s is solution. 
-  // Assign your row1 rook. choose coordinates 0,0 to be 1. 
-  // iterate through each row, index r, in order to cover the board. One rook piece per one row is assigned.
-  //    at every position in this row (new loop thru index in row, i), check if there are conflicts (only row and col conflicts)
-  //    if either conflict true, do nothing
-  //    if conflict is false, reassign position square from 0 to 1
-  // close the loop
-
-
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution; // assign solution to board
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
